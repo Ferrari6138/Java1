b05da188c1d242d8fddff9cfd3b442c72674a8c0
Gabriel Ferrari <gabrielferrari446@gmail.com>
2023-08-08 23:12:24 -0300

    reseting

---
+++ Aula3-08-08-23/Aula3/projetoLoginSimples/src/main/java/registraDados.java
@@ -0,0 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+public class registraDados {
+
+	public String escrevernoArquivo(String nome, String email, String cpf) {
+		
+		String enderecoArquivo = "D:\\Users\\gabri\\eclipse-workspace\\projetoLoginSimples\\src\\main\\webapp\\META-INF\\login.txt";
+		String escrita = "NOME: " +nome+ "CPF: "+ cpf +"EMAIL: " +email;
+		
+		try {
+			BufferedWriter buWriter = new BufferedWriter(new FileWriter(enderecoArquivo, true));
+			buWriter.append(escrita + "\n");
+			buWriter.close();
+			
+			System.out.println("Deu bom");
+			return "Deu bom no cadastro";
+		} catch (Exception e) {
+			System.out.println("Deu ruim no cadastro");
+			return "Deu ruim no cadastro";
+		}
+		
+		
+		
+	}
+	
+}
