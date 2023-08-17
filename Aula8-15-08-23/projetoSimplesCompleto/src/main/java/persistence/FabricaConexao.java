ad02ba9842fdaba50af0a545ceee5a2c1e191445
Gabriel Ferrari <gabrielferrari446@gmail.com>
2023-08-16 21:28:13 -0300

    Aula8 Exercicio

---
+++ Aula8-15-08-23/projetoSimplesCompleto/src/main/java/persistence/FabricaConexao.java
@@ -0,0 +1,27 @@
+package persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class FabricaConexao {
+	private String USUARIO = "root";
+	private String SENHA = "root";
+	
+	private static String BANCO_URL_USUARIOS = "jdbc:mysql://localhost:3306/db_usuarios?useTimezone=true&serverTimezone=UTC";
+	
+	public Connection criarConexoBancoUsuario() {
+		Connection conexao = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			conexao = DriverManager.getConnection(BANCO_URL_USUARIOS, USUARIO, SENHA);
+			System.out.println("Conexão Realizada");
+	
+		} catch (Exception e) {
+			System.out.println("Erro na conexão");
+		}
+		return conexao;
+	}
+
+}
